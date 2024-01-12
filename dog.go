c251452f21ab4a03533b2cc6bbc20846a55a239c
zbershadsky <z.bershadsky@gmail.com>
2024-01-12 16:57:39 +0200

    repo init

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func WhenGrownUp(s string) string {
+	return "When the puppy growns up it says: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zbershadsky/dog
+
+go 1.21.5
